23efa2d642acf3d39e15a1936704093e0a14f49b
Bray Moll <bkm82@nau.edu>
2024-10-14 18:14:10 -0700

    add write cordinates implementation

--- src/pyflowvc/main.py
+++ src/pyflowvc/main.py
@@ -14,8 +14,10 @@ def bray_add_two(a):
 
 
 def write_coordinates(mesh, coord_file):
-    """Write cordinates (x, y, z) to the specified file"""
-    pass
+    """Write coordinates (x, y, z) to the specified file."""
+    with open(coord_file, "w") as f:
+        for point in mesh.points:
+            f.write(f"{point[0]}\n{point[1]}\n{point[2]}\n")
 
 
 def main():
